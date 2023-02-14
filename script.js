e9d80c7571d66ac6a1f7b77c48a16bb9f292c888
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-02-14 13:06:20 +0300

    Add from elements to js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 //Acquire elements
 const addButton = document.getElementById('addBtn');
 const overlay = document.getElementById('overlay');
+const addform = document.getElementById('add-Book-Form');
+
+//Acquire form elements
+const title = document.getElementById('bookName');
+const author = document.getElementById('bookAuthor');
+const pages = document.getElementById('pages');
+const readStat = document.getElementById('read?');
 
 //add event listeners
 addButton.addEventListener('click', ()=> {
@@ -11,5 +18,9 @@ overlay.addEventListener('click', (event)=> {
     if (event.target.id == 'overlay') {
         overlay.style.display = 'none';
     }
+});
 
+addform.addEventListener('submit', ()=> {
+    
+    addform.reset();
 });
