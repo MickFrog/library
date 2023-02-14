3b77e11f65d823ed48e7c0f37b3bbc10381690d5
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-02-14 12:08:30 +0300

    change form identifier to id

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 const addButton = document.getElementById('addBtn');
 const overlay = document.getElementById('overlay');
 
-console.log(overlay);
-
 //add event listeners
 addButton.addEventListener('click', ()=> {
     overlay.style.display = 'flex';
