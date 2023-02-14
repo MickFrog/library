8f2db8fb452c3065e362d8dbff66c1daa6a25326
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-02-14 11:55:50 +0300

    Add overlay display on add button click

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //Acquire elements
 const addButton = document.getElementById('addBtn');
+const overlay = document.getElementById('overlay');
+
+console.log(overlay);
 
 //add event listeners
 addButton.addEventListener('click', ()=> {
-    
+    overlay.style.display = 'flex';
 });
